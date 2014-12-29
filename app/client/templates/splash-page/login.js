4a121fd67841eb6a8cf2682a8eab22e94e18fdd3
Henry Walden <henry@grout.io>
2014-12-29 13:24:06 +1100

    Add user login event on submit

--- app/client/templates/splash-page/login.js
+++ app/client/templates/splash-page/login.js
@@ -1,3 +1,61 @@
-/**
- * Created by dev on 28/12/14.
- */
+//login form error Session
+Template.loginForm.created = function(){
+    Session.set('loginErrors', {});
+};
+
+//using our template. set the error message
+Template.loginForm.helpers({
+    errorMessage : function(field) {
+        return Session.get('loginErrors')[field];
+    },
+    errorClass: function(field) {
+        return !!Session.get('loginErrors')[field] ? 'has-error' : '';
+    }
+});
+
+//sign up form event handler
+Template.loginForm.events({
+    'submit #login-form' : function(e){
+        e.preventDefault();
+
+        var loginForm = $(e.currentTarget),
+            email = trimInput(loginForm.find('#email').val().toLowerCase()),
+            password = loginForm.find('#password').val(),
+
+        //to set out session errors
+            loginFields = {
+                email : email,
+                password: password
+            },
+
+        //check for empty form fields
+        errors = validate(loginFields);
+
+        if(errors.email || errors.password){
+            return Session.set('loginErrors', errors);
+        }
+
+
+        //if email and password fields are not empty, we will check within our accounts collection
+        //for correct email and password match
+
+        Meteor.loginWithPassword(email, password, function(err) {
+            if (err) {
+                errors.email = err.reason,
+                errors.password = err.reason;
+                return Session.set('loginErrors', error);
+
+            } else {
+                //todo add router.go to user kanban board
+                console.log('Welcome back Meteorite!');
+
+                //refresh Session state
+                return Session.set('loginErrors', {});
+            }
+        });
+
+
+        //keeps complier happy?
+        return false;
+    }
+});
