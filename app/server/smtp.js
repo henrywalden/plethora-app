9c2dba0576baf84f180e974cca809986d80b1af6
Henry Walden <henry@grout.io>
2014-12-30 20:23:16 +1100

    Add smtp server for email routing

--- app/server/smtp.js
+++ app/server/smtp.js
@@ -1,3 +1,10 @@
-/**
- * Created by dev on 30/12/14.
- */
+/*email reset*/
+Meteor.startup(function () {
+    var smtp = {
+        username: 'suddha1@gmail.com',   // eg: server@gentlenode.com
+        password: 'sh0xwavE55**',   // eg: 3eeP1gtizk5eziohfervU
+        server:   'smtp.gmail.com',  // eg: mail.gandi.net
+        port: 25
+    };
+    process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+});
