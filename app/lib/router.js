7981968c110dd140e03f29aed52c9a3ec3e477de
Henry Walden <henry@grout.io>
2014-12-30 20:24:14 +1100

    Add seperate routing template to for splash, reset and kanban

--- app/lib/router.js
+++ app/lib/router.js
@@ -1,9 +1,4 @@
-//splash page routing
-Router.route('/', {
-    layoutTemplate: 'splash',
-    name: 'home'
-});
-
+//global router config
 Router.configure({
     layoutTemplate: 'layout',
     loadingTemplate: 'loading',
@@ -12,7 +7,25 @@ Router.configure({
 });
 
 
-//todo route to board with username on signin/login
+//splash page routing
+Router.route('/', {
+    layoutTemplate: 'splash',
+    name: 'home'
+});
+
+//reset password - send email route
+Router.route('/reset', {
+    layoutTemplate: 'Reset',
+    name: 'ForgotPassword'
+});
+
+//reset password page route
+Router.route('/reset-password', {
+    layoutTemplate: 'Reset',
+    name: 'ResetPassword'
+});
+
+
 Router.route('/welcome/', {
     name: 'kanban'
 });
