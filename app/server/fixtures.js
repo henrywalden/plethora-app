92291d7e58af2c0968ae83fd8dc189c596d6f9ba
Henry Walden <henry@grout.io>
2014-12-31 16:15:32 +1100

    Add fixtures for boards

--- app/server/fixtures.js
+++ app/server/fixtures.js
@@ -1,3 +1,32 @@
-/**
- * Created by dev on 31/12/14.
- */
+//kanban board fixtures
+
+if (Boards.find().count() === 0) {
+    // create two users
+    var henry = Meteor.users.insert({
+        profile: {username: 'HenryQ' }
+    });
+    var henryId = Meteor.users.findOne(henry);
+
+    Boards.insert({
+        userId: henryId._id,
+        boardName: "Hello World"
+    });
+
+    Boards.insert({
+        userId: henryId._id,
+        boardName: "HrldWorELo"
+    });
+
+    Boards.insert({
+        userId: henryId._id,
+        boardName: "HolWdeolrld"
+    });
+
+    //create ten more boards
+    for(var i= 0; i< 10; i++) {
+        Boards.insert({
+            userId: henryId._id,
+            boardName: 'Test post #' + i
+        });
+    }
+}
