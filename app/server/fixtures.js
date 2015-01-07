81099207a8d8e28195159da303aff327e6481978
Henry Walden <henry@grout.io>
2015-01-08 07:33:59 +1100

    Add name, cards and created properties for kanban fixtures

--- app/server/fixtures.js
+++ app/server/fixtures.js
@@ -1,32 +1,45 @@
 //kanban board fixtures
 
 if (Boards.find().count() === 0) {
+
     // create two users
     var henry = Meteor.users.insert({
-        profile: {username: 'HenryQ' }
-    });
-    var henryId = Meteor.users.findOne(henry);
+        profile: {
+            username: 'HenryQ',
+            password: "henry12"
+        }
+    }),
+    henryId = Meteor.users.findOne(henry),
+    now = new Date().getTime();
 
     Boards.insert({
         userId: henryId._id,
-        boardName: "Hello World"
+        boardName: "Hello World",
+        cards: [],
+        created: new Date(now - 7 * 3600 * 1000)
     });
 
     Boards.insert({
         userId: henryId._id,
-        boardName: "HrldWorELo"
+        boardName: "HrldWorELo",
+        cards: [],
+        created: new Date(now - 5 * 3600 * 1000)
     });
 
     Boards.insert({
         userId: henryId._id,
-        boardName: "HolWdeolrld"
+        boardName: "HolWdeolrld",
+        cards: [],
+        created: new Date(now - 3 * 3600 * 1000)
     });
 
     //create ten more boards
     for(var i= 0; i< 10; i++) {
         Boards.insert({
             userId: henryId._id,
-            boardName: 'Test post #' + i
+            boardName: 'Test post #' + i,
+            cards: [],
+            created: new Date()
         });
     }
 }
