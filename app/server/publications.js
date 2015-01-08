32864db5975ba00f1421bb2a66d90c6158fe2c82
Henry Walden <henry@grout.io>
2015-01-08 19:03:12 +1100

    Add broken xml parse

--- app/server/publications.js
+++ app/server/publications.js
@@ -1,1 +1,14 @@
 /* meteor publish definitions */
+
+//publish kanban board to user
+Meteor.publish('boards', function(id) {
+    //check(id,  String);
+    return Boards.find({ userId: this.userId });
+});
+
+
+//publish searches to user
+Meteor.publish('searches', function(id) {
+    //check(id,  String);
+    return Searches.find({ userId: this.userId });
+});
