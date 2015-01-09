800f53365a0b85dd489eac904b2e31c073f0d674
Henry Walden <henry@grout.io>
2015-01-09 15:47:19 +1100

    Add publish to bookSearch and use googleApi for book searches

--- app/server/publications.js
+++ app/server/publications.js
@@ -8,7 +8,30 @@ Meteor.publish('boards', function(id) {
 
 
 //publish searches to user
-Meteor.publish('searches', function(id) {
-    //check(id,  String);
-    return Searches.find({ userId: this.userId });
+Meteor.publish('booksSearch', function(query) {
+    var self = this;
+
+    try {
+        var response = HTTP.get('https://www.googleapis.com/books/v1/volumes', {
+            params: {
+                q: query
+            }
+        });
+
+        _.each(response.data.items, function(item) {
+            var doc = {
+                thumb: item.volumeInfo.imageLinks.smallThumbnail,
+                title: item.volumeInfo.title,
+                link: item.volumeInfo.infoLink,
+                snippet: item.searchInfo && item.searchInfo.textSnippet
+            };
+
+            self.added('books', Random.id(), doc);
+        });
+
+        self.ready();
+
+    } catch(error) {
+        throw new Meteor.Error('server timeout', error);
+    }
 });
