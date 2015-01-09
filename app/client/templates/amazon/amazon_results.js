bc72f9e0843e85fa14528f3ebf96ae854d698fea
Henry Walden <henry@grout.io>
2015-01-09 15:50:55 +1100

    Add line on main kanban for no new boards added yet

--- app/client/templates/amazon/amazon_results.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by aspnet2 on 9/01/2015.
- */
---
+++ app/client/templates/google-api/google_api_search.js
@@ -0,0 +1,36 @@
+
+//receive Session
+Tracker.autorun(function() {
+    if (Session.get('queryParams')) {
+        var searchHandle = Meteor.subscribe('booksSearch', Session.get('queryParams'));
+        Session.set('searching', ! searchHandle.ready());
+    }
+});
+
+//too books helper search our Searches Collection and return current user's only searches
+Template.amazonSearchPage.helpers({
+    books:  function() {
+        return Books.find();
+    },
+
+    searching: function() {
+        return Session.get('searching')
+    }
+});
+
+
+
+
+//searchResults: function() {
+//    var userQuery = Session.get('queryParams');
+//
+//    Meteor.call('amazonSearch', userQuery, function(error,  result){
+//        if(error) {
+//            console.log("amazonSearch call error");
+//            return throwError(error);
+//        }
+//
+//        return result;
+//
+//    });
+//},
