71d2a63404c644db11da7cbedfb609f0506d727b
Henry Walden <henry@grout.io>
2015-01-09 15:44:26 +1100

    Add environment variables to smtp server

--- app/server/smtp.js
+++ app/server/smtp.js
@@ -1,10 +1,7 @@
 /*email reset*/
 Meteor.startup(function () {
-    var smtp = {
-        username: 'suddha1@gmail.com',   // eg: server@gentlenode.com
-        password: 'sh0xwavE55**',   // eg: 3eeP1gtizk5eziohfervU
-        server:   'smtp.gmail.com',  // eg: mail.gandi.net
-        port: 25
-    };
-    process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+    var smtp = process.env;
+
+    process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.USERNAME) + ':' + encodeURIComponent(smtp.PASSWORD) + '@' + encodeURIComponent(smtp.SERVER) + ':' + smtp.PORT;
+
 });
