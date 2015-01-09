3ce13e8239c6a7f95670385fcf9fc65a6c27e2aa
Henry Walden <henry@grout.io>
2015-01-09 18:56:26 +1100

    Remove search to google api from board title form

--- app/client/templates/includes/drop_down.js
+++ app/client/templates/includes/drop_down.js
@@ -12,31 +12,26 @@ Template.dropDownCreate.events({
         e.preventDefault();
         var createBoard = $(e.currentTarget),
             title = createBoard.find('#boardNewTitle').val(),
-            searchApi = createBoard.find('#boardNewSearch').val(),
+            //searchApi = createBoard.find('#boardNewSearch').val(),
 
             //to check for blank form fields
-            boardFields = {
-                title : title,
-                searchApi : searchApi
-            },
-            error = validateBoard(boardFields);
+            error = validateBoard(title);
 
         //display error if user enter empty field
         if(error) {
             return throwError(error.msg);
         } else {
-
-
             //call boardInsert method to insert board title
-            Meteor.call('boardsInsert', boardFields, function(error, result) {
+            Meteor.call('boardsInsert', title, function(error, result) {
                 //display error to user and abort
+                console.log("error level 2");
+                console.log(error);
                 if (error) {
                     return throwError(error.reason);
                 }
 
-                Session.set('queryParams', boardFields.searchApi);
-
-                Router.go('amazonSearchPage', {_id: result._id}, {query: 'q='+boardFields.searchApi});
+                //route to boards page to display books
+                Router.go('individualKanban', {_id: result._id});
 
                 //attempt to call searchInsert
                 //Meteor.call('searchesInsert', boardFields.searchApi, function(error, result) {
