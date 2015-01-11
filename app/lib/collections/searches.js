3ff38357c5229c40ba7d6286718a80731c81758c
Henry Walden <henry@grout.io>
2015-01-12 10:13:30 +1100

    Add searchInsert method on Search collection

--- app/lib/collections/searches.js
+++ app/lib/collections/searches.js
@@ -1,1 +1,23 @@
 Search = new Mongo.Collection('search');
+
+Meteor.methods({
+    searchesInsert: function (search){
+        check(this.userId, String);
+
+
+        var searches = {
+            userId : Meteor.userId(),
+            userSearch : search
+        };
+
+        var searchId = Search.insert(searches);
+
+
+        return {
+            _id : searchId,
+            query: search
+        }
+
+    }
+});
+
